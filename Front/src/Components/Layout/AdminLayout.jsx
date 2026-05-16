920eec910a8c810e07ce0bbf76555c743a463ed4
AmiraAshraf2 <stud21std@gmail.com>
2026-05-16 17:34:10 +0300

    Add admin sidebar and top navbar navigation UI
    Implemented the main dashboard navigation layout including:
    
    Added sidebar navigation with:
    Tickets
    Users
    Reports
    Settings

--- Front/src/Components/Layout/AdminLayout.jsx
+++ Front/src/Components/Layout/AdminLayout.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from './Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar/Sidebar'
 
 export default function AdminLayout() {
   return (
     <div>
-      
+      <Navbar />
+      <div className='d-flex'>
+        <Sidebar />
+        <Outlet />
+
+      </div>
+
     </div>
   )
 }
--- Front/src/Pages/Admin/Employees.jsx
+++ Front/src/Pages/Admin/Employees.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Employees() {
   return (
     <div>
-      
+      Employees
     </div>
   )
 }
